8677b496221226467315bf75648398c732bfaf98
Sophia Portela <sophianobreportela@gmail.com>
2025-03-16 23:59:35 -0300

    Revert "First commit - migration, repositories, domain and service layers"
    This reverts commit f2b9b1b99444d0d3165264b4e97233e064cafd93.

--- src/main/java/com/snportela/inventory_system/exceptions/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package com.snportela.inventory_system.exceptions;
-
-public class BusinessException extends RuntimeException {
-
-    public BusinessException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/snportela/inventory_system/exceptions/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.snportela.inventory_system.exceptions;
-
-public class NotFoundException extends BusinessException {
-
-    public NotFoundException() { super("Resource not found."); }
-
-}
